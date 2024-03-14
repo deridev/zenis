ab61ede65f6d1fcac508eeb6539a8b389f57dcff
DeriDev <derickkp21@gmail.com>
2024-03-13 23:50:44 -0300

    Go debug

--- zenis_common/src/config.rs
+++ zenis_common/src/config.rs
@@ -3,7 +3,7 @@ use std::ops::RangeInclusive;
 pub const BOT_IDS: &[u64] = &[1215409249262379018];
 pub const DEBUG_GUILD_ID: u64 = 562364424002994189;
 
-pub const DEBUG: bool = false;
+pub const DEBUG: bool = true;
 
 pub const NAME_SIZE: RangeInclusive<usize> = 1..=16;
 pub const DESCRIPTION_SIZE: RangeInclusive<usize> = 1..=800;
