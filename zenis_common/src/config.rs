e2987fec587a35cc48f82c11fafa8e46ce112143
DeriDev <derickkp21@gmail.com>
2024-03-23 21:39:31 -0300

    increase some numbers

--- zenis_common/src/config.rs
+++ zenis_common/src/config.rs
@@ -5,12 +5,12 @@ pub const DEBUG_GUILD_ID: u64 = 562364424002994189;
 
 pub const DEBUG: bool = false;
 
-pub const ARENA_NAME_SIZE: RangeInclusive<usize> = 1..=32;
+pub const ARENA_NAME_SIZE: RangeInclusive<usize> = 1..=64;
 pub const ARENA_DESCRIPTION_SIZE: RangeInclusive<usize> = 1..=300;
 
-pub const NAME_SIZE: RangeInclusive<usize> = 1..=16;
-pub const DESCRIPTION_SIZE: RangeInclusive<usize> = 1..=800;
-pub const INTRODUCTION_MESSAGE_SIZE: RangeInclusive<usize> = 1..=256;
+pub const NAME_SIZE: RangeInclusive<usize> = 1..=48;
+pub const DESCRIPTION_SIZE: RangeInclusive<usize> = 1..=1024;
+pub const INTRODUCTION_MESSAGE_SIZE: RangeInclusive<usize> = 1..=312;
 
 pub const CREATE_AGENT_PRICE: i64 = 0;
 pub const PUBLISH_AGENT_PRICE: i64 = 0;
